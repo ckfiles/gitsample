f7607e32a62bd38b5186e5fe8ff56983d3a5d4dc
ckayfile <books.karthikeyanc@gmail.com>
2018-11-26 12:55:44 +0530

    added getter setter method

--- src/main/java/Mudhal.java
+++ src/main/java/Mudhal.java
@@ -3,4 +3,12 @@ public class Mudhal {
     private String firstName;
     private String lastName;
 
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
